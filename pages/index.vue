e0eca0bfd16bd30ed298e722f0e113e73dd98c0a
Filipe Borges <f.borges@rabbit-mobile.de>
2022-12-15 17:48:52 +0000

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,47 @@
+<template>
+    <div :style="{ 
+        backgroundImage: `url('/img/bg.jpg')`,
+        width: '100%',
+        height: '100%'
+    }">
+        <div :style="{ 
+            width: '80%',
+            margin: 'auto',
+            height: '100%'
+        }">
+        
+            <br>    
+            <div :style="{ 
+                width: '100%',
+                height: '70px',
+                backgroundColor: '#ffffff0f',
+                borderRadius: '10px',
+                backdropFilter: 'blur(10px)',
+                paddingLeft: '8px',
+            }">
+                <div v-for="menu in menus" :style="{
+                    borderRadius: '50%',
+                    width: '50px',
+                    height: '50px',
+                    backgroundColor: '#ffffff',
+                    marginTop: '10px',
+                    marginRight: '7px',
+                    float: 'left'
+                }"/>
+
+            </div>
+        </div>
+
+    </div>
+</template>
+
+
+<script setup lang="ts">
+
+const menus = ref([
+    'img',
+    'img',
+    'img'
+])
+
+</script>
